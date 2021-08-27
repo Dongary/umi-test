5103134305f2b7e7369e10f22ba9ca0935fb4707
Dongary <991537024@qq.com>
2021-08-27 18:32:52 +0800

    first umi build

--- .umirc.ts
+++ .umirc.ts
@@ -1,11 +1,23 @@
 import { defineConfig } from 'umi';
-
 export default defineConfig({
+  dva: {
+    hmr: true,
+  },
+  dynamicImport: {},
   nodeModulesTransform: {
     type: 'none',
   },
   routes: [
-    { path: '/', component: '@/pages/index' },
+    {
+      name: '个人中心',
+      path: '/',
+      component: '@/pages/AccountCenter',
+    },
+    {
+      name: '监控页',
+      path: '/dash',
+      component: '@/pages/DashboardMonitor',
+    },
   ],
   fastRefresh: {},
 });
